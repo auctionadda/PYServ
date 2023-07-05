f3d023520e78b047f70b88709f3bbaedbf51c164
ankesh <ankesh@ggs.com>
2023-07-05 15:57:25 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def handle_post_request():
     base64_data = request.get_data(as_text=True)
+
+    print("Received base64 data:", base64_data)
     
     # Process the base64 data here
     # For example, you can save it to a file, perform some computation, etc.
@@ -21,9 +23,11 @@ def handle_post_request():
             decoded_data = base64.b64decode(base64_data)
             
             # Save the decoded data to a file
-            filename = "data.txt"
+            filename = f"RecivedData/data.txt"
             with open(filename, "wb") as file:
                 file.write(decoded_data)
+
+            print("File saved to:", filename)
             
             response = "Data received and saved to file: " + filename
         except base64.binascii.Error as e:
@@ -34,4 +38,4 @@ def handle_post_request():
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
