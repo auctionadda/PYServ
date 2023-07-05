8da07ef8ed94244d3cf27348909f7adc5ff39632
ankesh <ankesh@ggs.com>
2023-07-05 14:44:54 +0530

    updates

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
-import requests
+from flask import Flask, request
 
-url = "https://pyserv.onrender.com"
-base64_data = "your_base64_data_here"
+app = Flask(__name__)
 
-response = requests.post(url, data=base64_data)
+@app.route('/', methods=['POST'])
+def handle_post_request():
+    base64_data = request.get_data(as_text=True)
+    
+    # Process the base64 data here
+    # For example, you can save it to a file, perform some computation, etc.
+    # Replace the following lines with your desired logic
+    
+    if base64_data:
+        response = "Data received and processed successfully."
+    else:
+        response = "No data received."
+    
+    return response
 
-if response.status_code == 200:
-    print("Received:", response.text)
-else:
-    print("Request failed with status code:", response.status_code)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
