fbe00f5a374a3f37ca611cf1a90562a091f88c74
ankesh <ankesh@ggs.com>
2023-07-06 15:07:34 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,45 @@
 from flask import Flask, request
+import base64
+import os
+import urllib.parse
 
 app = Flask(__name__)
 
+# Counter for assigning unique IDs
+id_counter = 0
+
 @app.route('/', methods=['POST'])
 def handle_post_request():
-    message = request.get_data(as_text=True)
+    global id_counter
 
-    print("Received message:", message)
+    # Generate a unique ID for the client
+    client_id = str(id_counter)
+    id_counter += 1
 
-    # Process the message here
-    # Replace the following lines with your desired logic
+    # Get the base64 string from the request
+    message = request.form.get('base64_data')
+    message = urllib.parse.unquote(message)
+    print("Received message encoded:", message)
 
-    if message:
-        response = "A Message received: " + message
-    else:
-        response = "No message received."
+    # Remove the "data:image/png;base64," prefix
+    prefix = "data:image/png;base64,"
+    if message.startswith(prefix):
+        message = message[len(prefix):]
+
+    # Save the base64 string as an image
+    try:
+        # Create the inputimages folder if it doesn't exist
+        if not os.path.exists('inputimages'):
+            os.makedirs('inputimages')
+
+        # Save the image with a unique filename
+        filename = f"inputimages/image_{client_id}.png"
+        with open(filename, 'wb') as image_file:
+            image_file.write(base64.b64decode(message))
+
+        response = "Message received. Image saved as: " + filename
+    except base64.binascii.Error as e:
+        response = "Error decoding base64 string: " + str(e)
 
     return response
 
